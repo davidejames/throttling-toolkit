290edfa3dc8916f3da8fac1cc2793b43fa2acbcd
David E. James <david.eugene.james@protonmail.com>
2024-11-02 16:28:50 -0400

    + throttling test

--- src/throttling_toolkit/simple.py
+++ src/throttling_toolkit/simple.py
@@ -2,6 +2,9 @@ import time
 import functools
 
 
+class Defaults:
+    TIME_FUNC = time.time
+
 class ThrottlingError(Exception):
     pass
 
@@ -11,12 +14,15 @@ def throttle(max_rate=5):
     def decorator(func):
 
         call_rate = 0
-        prev_call_time = time.time()
+        prev_call_time = Defaults.TIME_FUNC()
 
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
+            nonlocal call_rate
+            nonlocal prev_call_time
+
             call_rate      += 1
-            now             = time.time()
+            now             = Defaults.TIME_FUNC()
             call_rate       = max(0, call_rate - (now - prev_call_time))
             prev_call_time  = now
 
