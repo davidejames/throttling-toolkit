dbdd082061cbeb9ae8de7715240ba1389bb14312
David E. James <david.eugene.james@protonmail.com>
2024-11-02 16:42:14 -0400

    + more tests, dynamic init

--- src/throttling_toolkit/simple.py
+++ src/throttling_toolkit/simple.py
@@ -14,15 +14,19 @@ def throttle(max_rate=5):
     def decorator(func):
 
         call_rate = 0
-        prev_call_time = Defaults.TIME_FUNC()
+        prev_call_time = None
 
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
             nonlocal call_rate
             nonlocal prev_call_time
 
+            now = Defaults.TIME_FUNC()
+
+            if prev_call_time is None:
+                prev_call_time = now
+
             call_rate      += 1
-            now             = Defaults.TIME_FUNC()
             call_rate       = max(0, call_rate - (now - prev_call_time))
             prev_call_time  = now
 
--- tests/test__simple.py
+++ tests/test__simple.py
@@ -14,26 +14,88 @@ def test__basic():
         pass
 
 
-def test__throttling():
+def test__throttling__1():
     current_time = 0
     def _fake_time_f():
         nonlocal current_time
         current_time += 0.1
         return current_time
 
+    @throttle()
+    def foo():
+        pass
+
     _save_time_f = Defaults.TIME_FUNC
     Defaults.TIME_FUNC = _fake_time_f
+
+    n = 0
+    try:
+        # we should see throttling at 6th call:
+        with pytest.raises(ThrottlingError):
+            for n in range(10):
+                print(f'n={n}, time={current_time}')
+                foo()
+                print(f'n={n}, time={current_time}')
+
+    except Exception as e:
+        raise
+
+    finally:
+        Defaults.TIME_FUNC = _save_time_f
+
+    # range starts at 0, so for 6th call n will be 5
+    assert n == 5
+
+
+def test__throttling__2():
+    current_time = 0
+    def _fake_time_f():
+        nonlocal current_time
+        current_time += 0.1
+        return current_time
 
     @throttle()
     def foo():
         pass
 
-    # we should see throttling at 6th call:
-    with pytest.raises(ThrottlingError):
-        for n in range(10):
+    _save_time_f = Defaults.TIME_FUNC
+    Defaults.TIME_FUNC = _fake_time_f
+
+    try:
+        for n in range(5):
             foo()
 
-    Defaults.TIME_FUNC = _save_time_f
+        current_time += 5
+
+        for n in range(5):
+            foo()
+
+    finally:
+        Defaults.TIME_FUNC = _save_time_f
+
+
+
+def test__throttling__3():
+    current_time = 0
+    def _fake_time_f():
+        nonlocal current_time
+        current_time += 0.1
+        return current_time
+
+    @throttle()
+    def foo():
+        pass
+
+    _save_time_f = Defaults.TIME_FUNC
+    Defaults.TIME_FUNC = _fake_time_f
+
+    try:
+        # we should see throttling at 6th call:
+        with pytest.raises(ThrottlingError):
+            for n in range(10):
+                foo()
+    finally:
+        Defaults.TIME_FUNC = _save_time_f
 
     # range starts at 0, so for 6th call n will be 5
     assert n == 5
