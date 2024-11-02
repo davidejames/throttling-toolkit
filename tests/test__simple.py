cdb58c1f63cc3f973be8a7f44a6b586ce300f8d8
David E. James <david.eugene.james@protonmail.com>
2024-11-02 16:44:42 -0400

    ~ update test for clarity

--- tests/test__simple.py
+++ tests/test__simple.py
@@ -62,40 +62,17 @@ def test__throttling__2():
     Defaults.TIME_FUNC = _fake_time_f
 
     try:
+        # call enough times to queue up a throttle
         for n in range(5):
             foo()
 
+        # simulate some time passing
         current_time += 5
 
+        # rate should reset, so we should be able to call 5 times again
         for n in range(5):
             foo()
 
     finally:
         Defaults.TIME_FUNC = _save_time_f
 
-
-
-def test__throttling__3():
-    current_time = 0
-    def _fake_time_f():
-        nonlocal current_time
-        current_time += 0.1
-        return current_time
-
-    @throttle()
-    def foo():
-        pass
-
-    _save_time_f = Defaults.TIME_FUNC
-    Defaults.TIME_FUNC = _fake_time_f
-
-    try:
-        # we should see throttling at 6th call:
-        with pytest.raises(ThrottlingError):
-            for n in range(10):
-                foo()
-    finally:
-        Defaults.TIME_FUNC = _save_time_f
-
-    # range starts at 0, so for 6th call n will be 5
-    assert n == 5
